54a42dbd05f19de66ce737fa4189eb835ea19a05
Pavle992 <pavle.vidanovic@gmail.com>
2017-05-15 02:23:37 +0200

    Kernel SVM classifier

--- preprocessing.py
+++ preprocessing.py
@@ -21,7 +21,7 @@ dataset = pd.read_csv('train.csv', sep=';')
 
 #print(dataset.head())
 
-print(dataset.info())
+#print(dataset.info())
 
 #Prepricessing categorical values
 
@@ -34,7 +34,7 @@ dataset['EDUCATION'] = dataset['EDUCATION'].fillna('university', axis = 0)
 countMarriage = dataset['MARRIAGE'].value_counts()
 dataset['MARRIAGE'] = dataset['MARRIAGE'].fillna('single', axis = 0)
 
-print(dataset.info())
+#print(dataset.info())
 
 # Birthdate to date preprocessing
 from datetime import date, datetime
@@ -53,7 +53,7 @@ imputer = Imputer(missing_values = np.nan, strategy="median", axis = 1)
 xx = imputer.fit_transform(dataset.iloc[:, 5])
 dataset.iloc[:, 5] = xx.flatten()
 
-print(dataset.info())
+#print(dataset.info())
 
 # Encoding categorical variables
 
@@ -75,7 +75,7 @@ y = X[:, -1]
 
 #Splitting the dataset into training and test set
 from sklearn.cross_validation import train_test_split
-X_train, X_test, Y_train, Y_test = train_test_split( x, y, test_size=0.25, random_state=0, stratify=y)
+X_train, X_test, y_train, y_test = train_test_split( x, y, test_size=0.25, random_state=0, stratify=y)
 
 #Feature scaling
 from sklearn.preprocessing import StandardScaler
@@ -83,11 +83,13 @@ sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 
-print(X_train)
-print(X_test)
+#print(X_train)
+#print(X_test)
 
 # Fitting classifier to the Training set
-# Create your classifier HERE
+from sklearn.svm import SVC
+classifier = SVC(kernel = 'rbf', random_state=0)
+classifier.fit(X_train, y_train)
 
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
@@ -102,41 +104,6 @@ print(cm)
 from sklearn.cross_validation import cross_val_score
 accuracies = cross_val_score(estimator=classifier, X=X_train, y=y_train, cv=10)
 
-print(accuracies.mean())
-print(accuracies.std())
+print(str(accuracies.mean()) + ' +/- ' + str(accuracies.std()))
 
-# Visualising the Training set results
-from matplotlib.colors import ListedColormap
-X_set, y_set = X_train, y_train
-X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
-                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
-plt.xlim(X1.min(), X1.max())
-plt.ylim(X2.min(), X2.max())
-for i, j in enumerate(np.unique(y_set)):
-    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Classifier (Training set)')
-plt.xlabel('Age')
-plt.ylabel('Estimated Salary')
-plt.legend()
-plt.show()
 
-# Visualising the Test set results
-from matplotlib.colors import ListedColormap
-X_set, y_set = X_test, y_test
-X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
-                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
-plt.xlim(X1.min(), X1.max())
-plt.ylim(X2.min(), X2.max())
-for i, j in enumerate(np.unique(y_set)):
-    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Classifier (Test set)')
-plt.xlabel('Age')
-plt.ylabel('Estimated Salary')
-plt.legend()
-plt.show()
