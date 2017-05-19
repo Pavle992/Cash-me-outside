36b04494d54c8c349b9e150bbaf1472df44eee98
Pavle992 <pavle.vidanovic@gmail.com>
2017-05-19 13:05:00 +0200

    Classifiers object ready to use

--- preprocessing.py
+++ preprocessing.py
@@ -74,11 +74,11 @@ sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 
-# # Applying LDA
-# from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-# lda = LinearDiscriminantAnalysis(n_components = 2)
-# X_train = lda.fit_transform(X_train, y_train)
-# X_test = lda.transform(X_test)
+# Applying LDA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+lda = LinearDiscriminantAnalysis(n_components = 2)
+X_train = lda.fit_transform(X_train, y_train)
+X_test = lda.transform(X_test)
 
 # Fitting classifier to the Training set
 from sklearn.svm import SVC
@@ -108,12 +108,12 @@ classifier.fit(X_train, y_train)
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
 
-# #Making the Confusion Matrix
-# from sklearn.metrics import confusion_matrix, f1_score
-# cm = confusion_matrix(y_test, y_pred)
-# f1 = f1_score(y_test, y_pred, average='micro')
-# print(cm)
-# print(f1)
+#Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix, f1_score
+cm = confusion_matrix(y_test, y_pred)
+f1 = f1_score(y_test, y_pred, average='micro')
+print(cm)
+print(f1)
 
 # Applying k-Fold Cross Validation
 from sklearn.model_selection import cross_val_score, StratifiedKFold
@@ -134,41 +134,4 @@ print(accuracies.std())
 # best_parameters = grid_search.best_params_
 # print(best_accuracy)
 # print(best_parameters)
-
-# # Visualising the Training set results
-# from matplotlib.colors import ListedColormap
-# X_set, y_set = X_train, y_train
-# X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
-#                      np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-# plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-#              alpha = 0.75, cmap = ListedColormap(('red', 'green')))
-# plt.xlim(X1.min(), X1.max())
-# plt.ylim(X2.min(), X2.max())
-# for i, j in enumerate(np.unique(y_set)):
-#     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-#                 c = ListedColormap(('red', 'green'))(i), label = j)
-# plt.title('SVC (Training set)')
-# plt.xlabel('PC1')
-# plt.ylabel('PC1')
-# plt.legend()
-# plt.show()
-
-# # Visualising the Test set results
-# from matplotlib.colors import ListedColormap
-# X_set, y_set = X_test, y_test
-# X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
-#                      np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-# plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-#              alpha = 0.75, cmap = ListedColormap(('red', 'green')))
-# plt.xlim(X1.min(), X1.max())
-# plt.ylim(X2.min(), X2.max())
-# for i, j in enumerate(np.unique(y_set)):
-#     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-#                 c = ListedColormap(('red', 'green'))(i), label = j)
-# plt.title('SVC (Test set)')
-# plt.xlabel('PC1')
-# plt.ylabel('PC2')
-# plt.legend()
-# plt.show()
-
 
--- proba.py
+++
@@ -1,8 +0,0 @@
-from Classifiers import Classifiers
-
-models = Classifiers()
-print(models.listClassifiers())
-if 'KNN1' not in models.models.keys():
-	print('netacno')
-else:
-	print('tacno')
